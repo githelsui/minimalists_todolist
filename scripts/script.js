77a23b8006cdf43e9acc0cb03cec324886dc1a3c
Githel <githelsuico@gmail.com>
2019-06-30 15:53:23 -0700

    new proj

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function showMessage(){
+  window.alert('The ture is Female!');
+}
+
+function showBtn(){
+ var btn = document.getElementById("click_me").style.display = "none";
+  var msg = 'fdfd';
+  btn.innerHTML = msg;
+// window.alert('jk');
+}
