0a3ddc74022a7cd87b5c084bb1104bb91a971396
Githel <githelsuico@gmail.com>
2019-06-30 21:32:13 -0700

    append works

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+
+const input = document.getElementById("box");
+
 function showMessage(){
   window.alert('The ture is Female!');
 }
@@ -11,7 +14,20 @@ function showBtn(){
 
 function hideBtn(){
   var btn = document.getElementsByClassName('click_me')[0];
-  btn.style.transition='0.8s';
+  btn.style.transition='0.5s';
   btn.style.opacity=0;
-  console.log("djk");
+}
+
+function addItem(){
+  var section = document.getElementsByClassName('section')[0];
+  var newTask = document.createElement("div");
+  var taskName = input.value;
+  newTask.className = "card";
+  newTask.innerText = taskName;
+  section.appendChild(newTask);
+  console.log("works");
+}
+
+function removeItem(){
+
 }
